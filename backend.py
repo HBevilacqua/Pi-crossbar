027774ff839b153e901d6a60d5819d0576a6672c
HBevilacqua <hugo.benjamin.bevilacqua@gmail.com>
2016-10-14 13:32:00 +0000

    frontend (caller) calls the add2 remote procedure

--- backend.py
+++ backend.py
@@ -2,29 +2,29 @@ from os import environ
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.task import LoopingCall
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
-# or: from autobahn.asyncio.wamp import ApplicationSession
+from led_lib import ledOnOff
 
-
+# This class presents our lib to the crossbar router
 class MyComponent(ApplicationSession):
-    @inlineCallbacks
+    # calls onJoin when it joins the realm1
+    @inlineCallbacks 
     def onJoin(self, details):
-        # publish an event every second. The event payloads can be
-        # anything JSON- and msgpack- serializable
-        def publish():
-            return self.publish(u'com.myapp.hello', 'Hello, world!')
-        LoopingCall(publish).start(1)
-
-        # a remote procedure; see frontend.py for a Python front-end
-        # that calls this. Any language with WAMP bindings can now call
-        # this procedure if its connected to the same router and realm.
+        # RPC, callee side
         def add2(x, y):
+            print "MyComponent.add2"
             return x + y
         yield self.register(add2, u'com.myapp.add2')
 
+        # other RPC (raspberry)
+        #def led_turn_on(status):
+        #    print "ledOnOff(status)"
+        #yield self.register(led_turn_on, u'com.myapp.let_turn_on')
 
 if __name__ == '__main__':
+    # retrieves the environment variables
+    env_var = environ.get("AUTOBAHN_DEMO_ROUTER", u"ws://127.0.0.1:8080/ws")
     runner = ApplicationRunner(
-        environ.get("AUTOBAHN_DEMO_ROUTER", u"ws://127.0.0.1:8080/ws"),
+        env_var,
         u"realm1",
     )
     runner.run(MyComponent)
--- webapp.py
+++ webapp.py
@@ -1,46 +1,38 @@
 from flask import Flask
 from crochet import setup, run_in_reactor, wait_for
-
-# this MUST be called _before_ any Autobahn or Twisted imports!
+# crochet.setup() MUST be called before any Autobahn or Twisted imports
 setup()
-
 from autobahn.twisted.wamp import Application  # noqa
 
-
-# our WAMP app
-#
+# global WAMP app
 wapp = Application()
 
-# this is a synchronous wrapper around the asynchronous WAMP code
-#
-
-
-@wait_for(timeout=1)
-def publish(topic, *args, **kwargs):
-    return wapp.session.publish(topic, *args, **kwargs)
-
-
-# our Flask app
-#
+# global Flask app
 app = Flask(__name__)
-app._visits = 0
 
+# this is a synchronous wrapper around the asynchronous WAMP code
+@wait_for(timeout=1)
+# RPC caller side:
+def call(name, *args, **kwargs):
+    return wapp.session.call(name, *args, **kwargs)
 
 @app.route('/')
 def index():
-    app._visits += 1
-    publish(u'com.example.on_visit', app._visits, msg="hello from flask")
-    return "Visit {}".format(app._visits)
+    x = 40
+    y = 2
+    kwargs = {
+        'x': x,
+        'y': y,
+    }
+    # call the "add2" remote procedure
+    result = call(u'com.myapp.add2', **kwargs)
+    print "add result:", result
+    return "x: %s, y: %s, result: %s" % (x, y, result)
 
 
 if __name__ == '__main__':
-    import sys
-    import logging
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-
     # this will start the WAMP app on a background thread and setup communication
     # with the main thread that runs a (blocking) Flask server
-    #
     @run_in_reactor
     def start_wamp():
         wapp.run(u"ws://127.0.0.1:8080/ws", u"realm1", start_reactor=False)
