cc7f5202b30e5aa2adfd9b2723ceae011a644899
HBevilacqua <hugo.benjamin.bevilacqua@gmail.com>
2016-11-07 18:08:30 +0100

    add command line parameters to backend procedures for multy backend management

--- backend.py
+++ backend.py
@@ -4,6 +4,7 @@ from os import environ
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from led_lib import ledOnOff
+import argparse
 import logging
 logging.basicConfig()
 
@@ -15,35 +16,43 @@ class Component(ApplicationSession):
 
     @inlineCallbacks
     def onJoin(self, details):
-        print("session attached")
 
-        def on_topic(title):
-            print("Got event: {}".format(title))
+	self.args1 = self.config.extra['args1']
+        print("backend[%s]: session attached" % self.args1)
+
+	def on_topic(title):
+            print("backend[%s] Got event: %s" % (self.args1, title))
 
         def platform():
-            print "backend platform()"
+            print "backend[%s] platform()" % self.args1
             return [system(), release()]
 
         def divide(a, b):
-            print "backend divide()"
+            print "backend[%s] divide()" % self.args1
             return a / b
 
         def led_turn_on(status):
-            print "ledOnOff()"
+            print "backend[%s] ledOnOff()" % self.args1
             ledOnOff(status)
 
-        yield self.subscribe(on_topic, u'com.myapp.topic')
-        yield self.register(platform, u'com.myapp.platform')
-        yield self.register(divide, u'com.myapp.divide')
-        yield self.register(led_turn_on, u'com.myapp.led_turn_on')
-        print("Procedures registered; ready for frontend.")
+        yield self.subscribe(on_topic, u'com.myapp.topic'+self.args1)
+        yield self.register(platform, u'com.myapp.platform'+self.args1)
+        yield self.register(divide, u'com.myapp.divide'+self.args1)
+        yield self.register(led_turn_on, u'com.myapp.led_turn_on'+self.args1)
+        print("backend[%s] Procedures registered; ready for frontend." % self.args1)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--id", default="0")
+    args = parser.parse_args()
+
     router_address = environ.get("ROUTER_ADDRESS", u"ws://127.0.0.1:8080/ws")
     print "router_address:", router_address
+
     runner = ApplicationRunner(
         unicode(router_address),
         u"realm1",
+        extra={'args1':args.id}
     )
     runner.run(Component)
